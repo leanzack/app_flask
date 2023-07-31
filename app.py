25c12d2b0490bf26aa4635bf0b628179c4698964
leanzack <138343724+leanzack@users.noreply.github.com>
2023-07-31 17:06:16 +0800

    last edit

--- app.py
+++ app.py
@@ -2,12 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
  
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('hello.html')
-
-
-@app.route("/greet")
-def greet():
-    return render_template("greet.html", name=request.args.get("name", "world"))
-
+    if request.method == "GET": 
+        return render_template('hello.html')
+    elif request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name", "world"))
